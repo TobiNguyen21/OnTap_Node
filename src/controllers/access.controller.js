da00cc9b957c1b5b2cd53c1956f88aec287aafe2
TobiNguyen <nat21102002@gmail.com>
2023-09-23 20:10:18 +0700

    section8: Make your api res use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,9 +2,17 @@
 
 const AccessService = require('../services/access.service');
 
+const { CREATED } = require('../core/success.response');
+
 class AccessController {
     signUp = async (req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new CREATED({
+            message: 'Regiseted OK!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send(res);
     }
 }
 
